25d9b808be22ba9a6407915dcdfdccfb21e0a635
kunal-ch <kunalch36@gmail.com>
2022-06-19 19:00:11 +0530

    Functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/home_page.dart';
 
+// function
 void main() {
   runApp(MyApp());
 }
@@ -10,8 +11,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bringVegetables(carrybag: true);
     return MaterialApp(
       home: HomePage()
     );
+  }
+
+  // method
+  // {} means its optional
+  bringVegetables({int rupees=20, required bool carrybag}) {
+    // Take cycle
+    // Go to market
+    // Pay
   }
 }
